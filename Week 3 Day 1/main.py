fcb95fe4b5cd5fac5a242163a69cbe94ab5a15f3
LeoOuyang-CooperUnion <louyang.stem@cooper.edu>
2026-07-20 14:30:37 -0400

    aba

--- Week 3 Day 1/main.py
+++ Week 3 Day 1/main.py
@@ -1,1 +1,15 @@
-print('bruh')
+### print('Hello World')
+Adjectives = ['Good', 'Great', 'Amazing', 'Stellar', 'Wonderful']
+
+def moodcheck(x):
+    if x in Adjectives:
+        print("That's amazing")
+    else:
+        print("Damn that's rough, I hope your day gets better")
+
+name = input("Yo what's your name? ")
+print(f"Wsg {name}, I am jarvis.")
+mood = input("How's your day been? ").capitalize()
+moodcheck(mood)
+
+
